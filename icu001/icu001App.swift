5f6bea891902a68aaae23a2bd033755e779e437d
HANJUN <59297616+ccooddiinngg@users.noreply.github.com>
2023-06-18 16:07:59 -0700

    Initial Commit

---
+++ icu001/icu001App.swift
@@ -0,0 +1,17 @@
+//
+//  icu001App.swift
+//  icu001
+//
+//  Created by Hanjun Kang on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct icu001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
